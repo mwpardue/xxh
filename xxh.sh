2259c3c9f104bb5c98be89e8adad9f335d9abea8
mwpardue <mwpardue@gmail.com>
2025-08-26 16:20:15 -0400

    fixed auto width and height calculations

--- xxh.sh
+++ xxh.sh
@@ -1,13 +1,11 @@
-#!/bin/bash
+#!/usr/local/bin/bash
 
-# Initialize a variable to track the presence of the -s option
 s_flag=0
 
-# Loop through the arguments using getopts
 while getopts "s" opt; do
   case $opt in
   s)
-    s_flag=1 # Set s_flag to 1 if -s is passed
+    s_flag=1
     ;;
   \?) ;;
   esac
@@ -15,26 +13,75 @@ done
 
 cache_file="/tmp/ssh_config_cache"
 hash_file="/tmp/ssh_config_hash"
-tw_file="/tmp/xxh_text_width"
-homedir=$HOME
-# columns=$(/usr/bin/tput cols)
-text_width=200
+tw_file="/tmp/ssh_config_tw"
 
-# Function to calculate MD5 hashes of the config files
+homedir=$HOME
+client_width=$(tmux display-message -p '#{client_width}')
+text_width=$((($client_width * 40) / 100))
+
+# Function to calculate hash of .ssh directory
 calculate_hash() {
   md5 ${homedir}/.ssh/config* 2>/dev/null | md5
+}
+
+# Build hosts list from .ssh directory
+list_hosts() {
+  local line=$2
+  local host
+  local desc
+
+  if [[ $line =~ ^Host.*#.* ]]; then
+    IFS=# read -r host desc <<<${line:5}
+    desc="${desc/_[A-Z]*_/}"
+    spaces=$(($text_width - ${#host} - ${#desc}))
+    space_string=$(printf '%*s' "$spaces")
+    row="${desc}${space_string}${host}"
+    list+=("$row")
+  fi
+
+  # Save the list to cache
+  printf "%s\n" "${list[@]}" >"$cache_file"
+
+  # Save the new hash
+  echo "$current_hash" >"$hash_file"
 }
 
 # Calculate current hash
 current_hash=$(calculate_hash)
 
+border_label="Secure Shell Connections"
+
+# Check if the cachefile exists and compare hashes
+if [[ -f "$cache_file" ]] && [[ "$text_width" == "$(cat "$tw_file")" ]] &&
+  [[ "$(cat "$hash_file")" == "$current_hash" ]]; then
+
+  # Cache is up to date, load the list from the cache
+  list=()
+  mapfile -t list <$cache_file
+else
+  # Cache is missing or hash doesn't match, rebuild list
+  declare -a list=()
+  border_label="Secure Shell Connections NEW"
+  echo "$text_width" >"$tw_file"
+
+  for file in ${homedir}/.ssh/config*; do
+    mapfile -t -C list_hosts -c 1 hosts <$file
+  done
+fi
+
+fzf_width=$(($text_width + 10))
+fzf_height=$((${#list[@]} + 6))
+client_height=$(($(tmux display-message -p '#{client_height}') - 6))
+
+((fzf_height > client_height)) && fzf_height=$client_height
+
 FMENU=(
   fzf
-  --tmux 68%,80%
+  --tmux $(echo $fzf_width),$(echo $fzf_height)
   --layout=reverse
   --border=bold
   --border=rounded
-  --border-label="Secure Shell Connections"
+  --border-label="${border_label}"
   --border-label-pos=center
   --margin=2%
   --multi
@@ -53,46 +100,6 @@ FMENU=(
   --prompt
 )
 
-# Check if the cache and hash files exist and compare hashes
-if [[ -f "$cache_file" ]] &&
-  [[ -f "$hash_file" ]] &&
-  [[ "$(cat "$hash_file")" == "$current_hash" ]] &&
-  [[ "$(cat "$tw_file")" == "$text_width" ]]; then
-  # Cache is up to date, load the list from the cache
-  list=()
-  while IFS= read -r line; do
-    list+=("$line")
-  done <"$cache_file"
-else
-  echo "$text_width" >"$tw_file"
-  # Cache needs updating
-  declare -a list=()
-
-  for file in ${homedir}/.ssh/config*; do
-    if [[ -f "$file" ]]; then
-      while IFS='#' read -r host desc; do
-        if [[ -n $desc ]]; then
-          host="${host#Host }"
-          desc=$(echo "$desc" | sed -E 's/_[A-Z]+_//g')
-          hostLength=${#host}
-          descLength=${#desc}
-          totalLength=$text_width
-          spaces=$((totalLength - hostLength - descLength))
-          spaces=$((spaces > 0 ? spaces : 0))
-          space_string=$(printf '%*s' "$spaces")
-          row="${desc}${space_string}${host}"
-          list+=("$row")
-        fi
-      done < <(grep "^Host" "$file" 2>/dev/null)
-    fi
-  done
-
-  # Save the list to cache
-  printf "%s\n" "${list[@]}" >"$cache_file"
-  # Save the new hash
-  echo "$current_hash" >"$hash_file"
-fi
-
 selected_row=$(printf "%s\n" "${list[@]}" | "${FMENU[@]}" "Select host: ")
 if [[ -n "$selected_row" ]]; then
 
@@ -103,8 +110,6 @@ if [[ -n "$selected_row" ]]; then
 
   for i in "${selection_array[@]}"; do
     hostname=$(awk '{print $NF}' <<<"$i")
-    # kitten @ launch --type=tab --copy-env --tab-title "${hostname}" ssh $hostname
-    # kitten @ set-colors --match=recent:0 /Users/mpardue/.config/kitty/Borland.conf
     tmux new-window -n "${hostname}" ssh $hostname
   done
 fi
